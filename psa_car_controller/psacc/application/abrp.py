e4994b8c0256060818fefe116ad68e0417cd93ea
Florian Emser <florian.emser@gmail.com>
2024-04-19 09:56:44 +0200

    Reduce ABRP data to miniumum so consumption calcul ation does not get messed up

--- psa_car_controller/psacc/application/abrp.py
+++ psa_car_controller/psacc/application/abrp.py
@@ -41,16 +41,8 @@ class Abrp:
 
                 tlm = {"utc": int(datetime.timestamp(energy.updated_at)),
                        "soc": energy.level,
-                       "speed": getattr(car.status.kinetic, "speed", None),
-                       "car_model": car.get_abrp_name(),
-                       "current": car.status.battery.current,
-                       "is_charging": energy.charging.status == "InProgress",
-                       "lat": car.status.last_position.geometry.coordinates[1],
-                       "lon": car.status.last_position.geometry.coordinates[0],
-                       "power": energy.consumption
+                       "car_model": car.get_abrp_name()
                        }
-                if ext_temp is not None:
-                    tlm["ext_temp"] = ext_temp
                 params = {"tlm": json.dumps(tlm), "token": self.token, "api_key": self.api_key}
                 response = requests.request("POST", self.url, params=params, proxies=self.proxies,
                                             verify=self.proxies is None, timeout=TIMEOUT_IN_S)
